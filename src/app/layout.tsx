3c068a1b5f9eecfe27fc38cf39425ec20cffbb92
atneon27 <aryan.tiwari.at27@gmail.com>
2024-12-12 16:39:37 +0530

    inital commit + added basic authentication

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "@/styles/globals.css";
+
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+export const metadata: Metadata = {
+  title: "SwiftMail",
+  description: "Prototype for a Minimalist Email Client powered by AI",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
